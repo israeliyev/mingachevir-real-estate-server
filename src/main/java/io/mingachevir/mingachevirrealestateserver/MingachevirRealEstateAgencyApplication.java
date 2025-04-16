97c10b1e732fc216597d219e4ad5ee40e9375590
israelAliyev <space.y@inbox.ru>
2025-04-16 17:39:35 +0400

    some changes

--- src/main/java/io/mingachevir/mingachevirrealestateserver/MingachevirRealEstateAgencyApplication.java
+++ src/main/java/io/mingachevir/mingachevirrealestateserver/MingachevirRealEstateAgencyApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-//@EnableJpaRepositories(basePackages = "io.mingachevir.mingachevirrealestateserver.repository")
+@EnableJpaRepositories(basePackages = "io.mingachevir.mingachevirrealestateserver.repository")
 //@EntityScan(basePackages = "io.mingachevir.mingachevirrealestateserver.model.entity")
 public class MingachevirRealEstateAgencyApplication {
 
